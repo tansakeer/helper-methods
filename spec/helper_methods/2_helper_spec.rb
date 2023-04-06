0ae894db171d9b65df426b5dfe03d0a25f3eb2a1
Ben Purinton <ben.purinton@gmail.com>
2023-04-06 13:25:15 -0800

    change link_to spec

--- spec/helper_methods/2_helper_spec.rb
+++ spec/helper_methods/2_helper_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "movies/index" do
   it "uses an embedded Ruby link_to helper method", points: 2 do
     template_name = "movies/index.html.erb"
-    template_path = Rails.root.join('app', 'views', template_name)
+    template_path = Rails.root.join("app", "views", template_name)
     template_contents = open(template_path).read
-    expect(template_contents).to match(/<%=.*link_to.*Show details.*%>/),
-      "Expected to find `link_to` method with text 'Show details' in movies/index.html.erb"
+    expect(template_contents).to match(/<%=.*link_to.*Add a new movie.*%>/),
+      "Expected to find `link_to` method with text 'Add a new movie' in movies/index.html.erb"
   end
 end
