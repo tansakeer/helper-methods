d9eb3a19bee1667c0308b5a0818b03a18d673a9d
Tan Sakeer <tansakeer@gmail.com>
2023-07-11 20:19:44 +0000

    Dropped Render statements where controller name matches and/or method name matches, render was modified or removed. Redirect_to remains as is.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new.html.erb"
   end
 
   def index
@@ -15,8 +13,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index.html.erb"
+      format.html
       end
     end
   end
@@ -27,8 +24,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/show.html.erb"
   end
 
   def create
@@ -40,7 +35,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movie_url, notice: "Movie created successfully."
     else
-      render template: "movies/new.html.erb"
+      render template: "new"
     end
   end
 
@@ -50,8 +45,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template:  "movies/edit.html.erb"
   end
 
   def update
