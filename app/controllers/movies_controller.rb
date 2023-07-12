9a11d52f57ee55ec9a6c47b52e2993b6e439ae3d
Tan Sakeer <tansakeer@gmail.com>
2023-07-12 16:36:27 +0000

    added Nested params hatch configuration both controller and viewer changed. Also removed "query_" and _box

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,10 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
-    matching_movies = Movie.all
-
-    @list_of_movies = matching_movies.order( created_at: :desc )
+    @movies= Movie.order( created_at: :desc )
 
     respond_to do |format|
       format.json do
@@ -17,20 +15,16 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where( id: the_id )
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @movie = Movie.new
+    @movie.title = params.fetch(:movie).fetch("title")
+    @movie.description = params.fetch(:movie).fetch("description")
 
-    if @the_movie.valid?
-      @the_movie.save
+    if @movie.valid?
+      @movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
       render template: "movies/new.html.erb"
@@ -38,19 +32,16 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    the_id = params.fetch(:id)
+    @movie = Movie.find(params.fetch(:id))
 
-    matching_movies = Movie.where( id: the_id )
-
-    @the_movie = matching_movies.first
   end
 
   def update
     the_id = params.fetch(:id)
     the_movie = Movie.where( id: the_id ).first
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    the_movie.title = params.fetch("title")
+    the_movie.description = params.fetch("description")
 
     if the_movie.valid?
       the_movie.save
@@ -61,8 +52,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where( id: the_id ).first
+    @movie = Movie.find(params.fetch(:id))
 
     the_movie.destroy
 
